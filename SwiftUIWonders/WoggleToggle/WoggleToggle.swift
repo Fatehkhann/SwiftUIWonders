776df2ca115eb04dbf69d84da4e21d95472708c1
Fateh Khan <fatehkhann@gmail.com>
2024-04-21 18:28:13 +0500

    Done with Toggle Button

---
+++ SwiftUIWonders/WoggleToggle/WoggleToggle.swift
@@ -0,0 +1,18 @@
+//
+//  Toggle.swift
+//  SwiftUIWonders
+//
+//  Created by Fateh Khan on 21/04/2024.
+//
+
+import SwiftUI
+
+struct Toggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toggle()
+}
