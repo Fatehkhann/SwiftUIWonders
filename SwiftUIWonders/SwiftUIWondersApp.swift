d6050b58c78ac5f0fa0761e395c55457029a879a
Fateh Khan <fatehkhann@gmail.com>
2024-04-21 16:09:06 +0500

    Initial Commit

---
+++ SwiftUIWonders/SwiftUIWondersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWondersApp.swift
+//  SwiftUIWonders
+//
+//  Created by Fateh Khan on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWondersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
